0e7948c9f5334ca3b09d334bee7ed803704540d8
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-19 14:26:14 +0300

    styled fingernail position and background

--- src/Hand.js
+++ src/Hand.js
@@ -6,23 +6,25 @@ export default function Hand() {
     const [color, setColor] = useState(randomColor())
     
     return (
-    <div className="wrapper">
-        <div className="one">
+    <div className="canvas">
+        <div className="wrapper">
+            <div className="one">
+                <div className="nail" style={{backgroundColor:`${color}`}}></div>
+            </div>
+            <div className="two">
             <div className="nail" style={{backgroundColor:`${color}`}}></div>
-        </div>
-        <div className="two">
-        <div className="nail" style={{backgroundColor:`${color}`}}></div>
-        </div>
-        <div className="three">
-        <div className="nail" style={{backgroundColor:`${color}`}}></div>
-        </div>
-        <div className="four">
-        <div className="nail" style={{backgroundColor:`${color}`}}></div>
-        </div>
-        <div className="five">
-        <div className="nail" style={{backgroundColor:`${color}`}}></div></div>    
-        <div className="six">
-            <button className="button" onClick={() => setColor(randomColor())}>Change Color</button>
+            </div>
+            <div className="three">
+            <div className="nail" style={{backgroundColor:`${color}`}}></div>
+            </div>
+            <div className="four">
+            <div className="nail" style={{backgroundColor:`${color}`}}></div>
+            </div>
+            <div className="five">
+            <div className="nail" style={{backgroundColor:`${color}`}}></div></div>    
+            <div className="six">
+                <button className="button" onClick={() => setColor(randomColor())}>Change Color</button>
+            </div>
         </div>
     </div>
     )
