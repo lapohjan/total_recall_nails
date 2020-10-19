a142c0f790fd50cd507d0528798f803567188faf
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-10-19 18:34:48 +0300

    add background image

--- src/Hand.js
+++ src/Hand.js
@@ -6,22 +6,22 @@ export default function Hand() {
     const [color, setColor] = useState(randomColor())
     
     return (
-    <div className="canvas">
+    <div>
         <div className="wrapper">
             <div className="one">
                 <div className="nail" style={{backgroundColor:`${color}`}}></div>
             </div>
             <div className="two">
-            <div className="nail" style={{backgroundColor:`${color}`}}></div>
+                <div className="nail" style={{backgroundColor:`${color}`}}></div>
             </div>
             <div className="three">
-            <div className="nail" style={{backgroundColor:`${color}`}}></div>
+                <div className="nail" style={{backgroundColor:`${color}`}}></div>
             </div>
             <div className="four">
-            <div className="nail" style={{backgroundColor:`${color}`}}></div>
+                <div className="nail" style={{backgroundColor:`${color}`}}></div>
             </div>
             <div className="five">
-            <div className="nail" style={{backgroundColor:`${color}`}}></div></div>    
+                <div className="nail" style={{backgroundColor:`${color}`}}></div></div>    
             <div className="six">
                 <button className="button" onClick={() => setColor(randomColor())}>Change Color</button>
             </div>
